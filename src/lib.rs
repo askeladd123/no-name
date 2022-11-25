46d904c7f916f8000f369ab358b89e43af8e95b8
Ask Sødal <asks@uia.no>
2022-11-25 12:39:53 +0100

    winit vindu funker på windows

---
+++ src/lib.rs
@@ -0,0 +1,34 @@
+#[cfg(target_arch="wasm32")]
+use wasm_bindgen::prelude::*;
+
+use winit::{
+    event::*,
+    event_loop::{ControlFlow, EventLoop},
+    window::WindowBuilder,
+};
+
+#[cfg_attr(target_arch="wasm32", wasm_bindgen(start))]
+pub fn run() {
+    let event_loop = EventLoop::new();
+    let window = WindowBuilder::new().build(&event_loop).unwrap();
+    
+    event_loop.run(move |event, _, control_flow| match event {
+        Event::WindowEvent {
+            ref event,
+            window_id,
+        } if window_id == window.id() => match event {
+            WindowEvent::CloseRequested
+            | WindowEvent::KeyboardInput {
+                input:
+                KeyboardInput {
+                    state: ElementState::Pressed,
+                    virtual_keycode: Some(VirtualKeyCode::Escape),
+                    ..
+                },
+                ..
+            } => *control_flow = ControlFlow::Exit,
+            _ => {}
+        },
+        _ => {}
+    });
+}
---
+++ src/main.rs
@@ -0,0 +1,6 @@
+use wgpu_test1::run;
+
+fn main() {
+    tracing_subscriber::fmt::init();
+    run();
+}
